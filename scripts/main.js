b3cda55ecbd8823e2c34101397db0b62e3a7e8c3
GruapeFruit <eminem.zima@gmail.com>
2023-01-29 21:50:12 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/first.jpg') {
+      myImage.setAttribute ('src','images/second.jpg');
+    } else {
+      myImage.setAttribute ('src','images/first.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Мне нужно твое имя.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Соси, ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Соси, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
+  
